a255bafad1e7ba385de88231eafcafc66f79e7f9
Brian Murphy <brian@murphy.dk>
2014-02-16 21:21:01 +0100

    create new tags properly

--- newtags.py
+++ newtags.py
@@ -15,27 +15,69 @@ def gettags(tagfile, filt = None):
 
     return tag
 
-def retag(tagmapfile, t):
-    f = open(tagmapfile, "r")
+def getmaps(mapfile, filt = None):
+    f = open(mapfile, "r")
 
+    map = {}
+    rmap = {}
     for l in f:
-        (newrev, v) = l.strip().split(':')
-        oldrevs = v.split()
-        tag = []
-        for r in oldrevs:
-            if t.has_key(r):
-                tag += t[r]
+        (o, n) = l.strip().split(':')
+        old = o.strip()
+        new = n.strip()
+        if map.has_key(old):
+            raise Exception("duplicate commit %s" % old)
+        if rmap.has_key(new):
+            print("duplicate new commit %s and %s map to %s" % (o, rmap[new], new))
+        map[old] = new
+        rmap[new] = old
 
-        if tag:
-            for ti in tag:
-                cmd = "git tag %s %s" % (ti, newrev)
-                ret = subprocess.call(cmd.split())
-                if ret != 0:
-                    print("command failed: %s=%d" % (cmd, ret))
-            
+    return map
 
 if len(sys.argv) < 3:
     raise Exception("Missing Argument: syntax is '%s <revinfo file> <tagfile>'")
 
-tags = gettags(sys.argv[2])
-retag(sys.argv[1], tags)
+def parse_config(configfile):
+    TAGLEN = 6
+    config =  {}
+    base = ""
+
+    f = open(configfile, "r")
+    for l in f:
+        if l[:TAGLEN] == "REPO: ":
+            config["REPO"] = l[TAGLEN:-1]
+        if l[:TAGLEN] == "REVN: ":
+            config["REVN"] = l[TAGLEN:-1].split()
+        if l[:TAGLEN] == "FILT: ":
+            d = l[TAGLEN:-1].split()
+            if not config.has_key("FILT"):
+                config["FILT"] = []
+            config["FILT"].append(d[0])
+        if l[:TAGLEN] == "BASE: ":
+            base = l[TAGLEN:-1]
+        if l[:TAGLEN] == "TPFX: ":
+            config["TPFX"] = l[TAGLEN:-1]
+
+    f.close()
+    return config
+
+cfg = parse_config("csw.cfg")
+
+cmd = "git --git-dir=%s/.git rev-list --first-parent %s" % (cfg["REPO"], cfg["REVN"][1])
+revs = subprocess.check_output(cmd.split()).splitlines()
+
+tags = gettags(sys.argv[1])
+
+for name in cfg["FILT"]:
+    tname = cfg["TPFX"]+name
+
+    map = getmaps(name + ".revinfo")
+
+    ctags = []
+    for rev in revs:
+        if tags.has_key(rev):
+            ctags += tags[rev]
+        if ctags and map.has_key(rev):
+            for tag in ctags:
+                cmd = "git --git-dir=%s tag %s %s" % (tname, tag, map[rev])
+                rc = subprocess.call(cmd.split())
+            ctags = []
