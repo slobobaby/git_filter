7389a058891efd4aff211f7701a504a39503db59
Brian Murphy <brian.murphy@cobham.com>
2014-02-18 15:20:58 +0100

    document that git filter can continue from a previous run

--- git_filter.c
+++ git_filter.c
@@ -778,7 +778,7 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         log("please specify the location of a filter configuration\n");
-        log("%s <filter config>\n", argv[0]);
+        log("%s <filter config> [continue]\n", argv[0]);
         exit(1);
     }
 
